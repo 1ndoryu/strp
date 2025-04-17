311bbd67c0d79cff71401486bedb53ef7e6b0594
1ndoryu <andoryyu@gmail.com>
2025-04-17 05:03:56 -0400

    *

--- src/js/newPost.js
+++ src/js/newPost.js
@@ -681,20 +681,6 @@
         moveLeftBtn.addEventListener('click', handleMoveFotoClick); // Nuevo handler
         actionsDiv.appendChild(moveLeftBtn);
 
-        // --- Botón Cambiar Foto (Existente) ---
-        const changeBtn = document.createElement('button');
-        changeBtn.type = 'button';
-        changeBtn.classList.add('btn-preview-action', 'btn-change-foto');
-        changeBtn.title = 'Cambiar foto';
-        changeBtn.setAttribute('aria-label', `Cambiar la foto ${filename}`);
-        changeBtn.dataset.filename = filename;
-        changeBtn.innerHTML = `
-            <svg data-testid="geist-icon" height="12" stroke-linejoin="round" viewBox="0 0 16 16" width="12" style="color: currentcolor;" aria-hidden="true">
-                <path fill-rule="evenodd" clip-rule="evenodd" d="M8.00002 1.25C5.33749 1.25 3.02334 2.73677 1.84047 4.92183L1.48342 5.58138L2.80253 6.29548L3.15958 5.63592C4.09084 3.91566 5.90986 2.75 8.00002 2.75C10.4897 2.75 12.5941 4.40488 13.2713 6.67462H11.8243H11.0743V8.17462H11.8243H15.2489C15.6631 8.17462 15.9989 7.83883 15.9989 7.42462V4V3.25H14.4989V4V5.64468C13.4653 3.06882 10.9456 1.25 8.00002 1.25ZM1.50122 10.8555V12.5V13.25H0.0012207V12.5V9.07538C0.0012207 8.66117 0.337007 8.32538 0.751221 8.32538H4.17584H4.92584V9.82538H4.17584H2.72876C3.40596 12.0951 5.51032 13.75 8.00002 13.75C10.0799 13.75 11.8912 12.5958 12.8266 10.8895L13.1871 10.2318L14.5025 10.9529L14.142 11.6105C12.9539 13.7779 10.6494 15.25 8.00002 15.25C5.05453 15.25 2.53485 13.4313 1.50122 10.8555Z" fill="currentColor"></path>
-            </svg>`;
-        changeBtn.addEventListener('click', handleChangeFotoClick);
-        actionsDiv.appendChild(changeBtn);
-
         // --- NUEVO: Botón Mover Derecha ---
         const moveRightBtn = document.createElement('button');
         moveRightBtn.type = 'button';
@@ -708,6 +694,20 @@
             </svg>`;
         moveRightBtn.addEventListener('click', handleMoveFotoClick); // Nuevo handler
         actionsDiv.appendChild(moveRightBtn);
+
+        // --- Botón Cambiar Foto (Existente) ---
+        const changeBtn = document.createElement('button');
+        changeBtn.type = 'button';
+        changeBtn.classList.add('btn-preview-action', 'btn-change-foto');
+        changeBtn.title = 'Cambiar foto';
+        changeBtn.setAttribute('aria-label', `Cambiar la foto ${filename}`);
+        changeBtn.dataset.filename = filename;
+        changeBtn.innerHTML = `
+            <svg data-testid="geist-icon" height="12" stroke-linejoin="round" viewBox="0 0 16 16" width="12" style="color: currentcolor;" aria-hidden="true">
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M8.00002 1.25C5.33749 1.25 3.02334 2.73677 1.84047 4.92183L1.48342 5.58138L2.80253 6.29548L3.15958 5.63592C4.09084 3.91566 5.90986 2.75 8.00002 2.75C10.4897 2.75 12.5941 4.40488 13.2713 6.67462H11.8243H11.0743V8.17462H11.8243H15.2489C15.6631 8.17462 15.9989 7.83883 15.9989 7.42462V4V3.25H14.4989V4V5.64468C13.4653 3.06882 10.9456 1.25 8.00002 1.25ZM1.50122 10.8555V12.5V13.25H0.0012207V12.5V9.07538C0.0012207 8.66117 0.337007 8.32538 0.751221 8.32538H4.17584H4.92584V9.82538H4.17584H2.72876C3.40596 12.0951 5.51032 13.75 8.00002 13.75C10.0799 13.75 11.8912 12.5958 12.8266 10.8895L13.1871 10.2318L14.5025 10.9529L14.142 11.6105C12.9539 13.7779 10.6494 15.25 8.00002 15.25C5.05453 15.25 2.53485 13.4313 1.50122 10.8555Z" fill="currentColor"></path>
+            </svg>`;
+        changeBtn.addEventListener('click', handleChangeFotoClick);
+        actionsDiv.appendChild(changeBtn);
 
         // --- Botón Eliminar Foto (Existente) ---
         const deleteBtn = document.createElement('button');
